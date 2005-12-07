5c86b381ec1afb332a1c7a2f2a289f23a1c2fb3b
Mike Gashler <boulzh@users.sourceforge.net>
2005-12-07 18:11:07 +0000

    more stuff

--- bin/dbg.bat
+++
@@ -1,5 +0,0 @@
-start IsotopeDebug.exe server c:\www
-wait 2
-start IsotopeDebug.exe
-wait 1
-start IsotopeDebug.exe
--- bin/rel.bat
+++
@@ -1,5 +0,0 @@
-start Isotope.exe server c:\www
-wait 2
-start Isotope.exe
-wait 1
-start Isotope.exe
